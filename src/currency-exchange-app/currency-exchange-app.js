1f288911e6c1b60d3ba488e017fb9dab2e6c3a91
Brendan M <brendan.maclean94@gmail.com>
2019-03-19 00:13:12 -0700

    Use app layout in root component

--- src/currency-exchange-app/currency-exchange-app.js
+++ src/currency-exchange-app/currency-exchange-app.js
@@ -1,4 +1,7 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/app-layout/app-layout.js';
+import './currency-exchange-layout';
 import './currency-sidebar.js';
 
 /**
@@ -12,12 +15,27 @@ class CurrencyExchangeApp extends PolymerElement {
         :host {
           display: grid;
         }
+        app-drawer {
+          --app-drawer-width: 350px;
+          grid-template-rows: auto;
+          border: 1px;
+          box-shadow: 5px 0 5px grey;
+        }
       </style>
-      <currency-sidebar
-        id="currency-sidebar"
-        class="leftpanel"
-        name="currency-sidebar"
-      />
+      <app-drawer-layout>
+        <app-drawer
+          id="currency-sidebar"
+          class="leftpanel"
+          name="currency-sidebar"
+          slot="drawer"
+        >
+        <paper-item>cunt</paper-item>
+        <paper-item>fuck</paper-item>
+        <paper-item>you</paper-item>
+        <paper-item>cunt</paper-item>
+        </app-drawer>
+        <currency-app-content />
+      </app-drawer-layout>
     `;
   }
 
