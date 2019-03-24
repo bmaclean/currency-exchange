120ceda172df779d343aa88c1f17b485708cd5de
Brendan M <brendan.maclean94@gmail.com>
2019-03-24 01:29:44 -0700

    Styling and chart property changes.

--- src/currency-exchange-app/currency-chart.js
+++ src/currency-exchange-app/currency-chart.js
@@ -12,10 +12,6 @@ class CurrencyChart extends PolymerElement {
         <style>
 			:host {
 			}
-			google-chart {
-				height: 100%;
-				width: 90%;
-			}
 			.title {
 				position: fixed;
 				height: 67px;
@@ -27,14 +23,14 @@ class CurrencyChart extends PolymerElement {
 		<paper-item class="title">
 			<paper-item-body>
 				<div>
-					<h1>Historical Rates</h1>
+					<h1>Trends</h1>
 				</div>
 			</paper-item-body>
 		</paper-item>
 		<google-chart 
 			type="line" 
 			options=[[graphOptions]]
-			data='[["Date", "Currency"], ["Mar 17", 122.3], ["Mar 13", 433.3]]'>
+			data='[[currencies]]'>
 		</google-chart>
     `;
 	}
@@ -51,17 +47,50 @@ class CurrencyChart extends PolymerElement {
 			},
 			graphOptions: {
 				type: Object,
+				notify: true,
+				reflectToAttribute: true,
 				value() {
 					return {
 						backgroundColor: '#37474f',
-						width: '1200',
-						height: '600',
+						width: window.innerWidth * .7,
+						height: window.innerWidth * .5,
 						chartArea: {
+							top: 25
 						},
+						hAxis: {
+							// title: 'Date',
+							titleTextStyle: {
+								color: 'white'
+							},
+							textStyle: {
+								color: 'white',
+							},
+						},
+						vAxis: {
+							// title: 'Currency Value',
+							titleTextStyle: {
+								color: 'white'
+							},
+							textStyle: {
+								color: 'white',
+							},
+						}
 					};
 				},
 			},
 		};
+	}
+
+	ready() {
+		super.ready();
+		window.addEventListener('resize', this._resize.bind(this))
+	}
+
+	_resize() {
+		this.graphOptions.width = window.innerWidth * 0.7;
+		this.graphOptions.height = window.innerWidth * 0.5;
+		this.notifyPath('graphOptions.width');
+		this.notifyPath('graphOptions.height');
 	}
 }
 
